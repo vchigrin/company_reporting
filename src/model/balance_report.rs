aa1ab27e17b6b3ae4d2ef68769cb2da967da8428
Vyacheslav Chigrin <vyacheslav.chigrin@izba.dev>
2026-01-31 22:05:23 +0300

    Implement Balance model types.

--- src/model/balance_report.rs
+++ src/model/balance_report.rs
@@ -1,4 +1,5 @@
 use super::money::Money;
+use eyre::{Result, eyre};
 
 // Необоротные активы.
 pub struct NonCurrentAssets {
@@ -70,4 +71,138 @@ pub struct BalanceReport {
     assets: Assets,
     equity: Equity,
     liabilities: Liabilities,
+}
+
+impl NonCurrentAssets {
+    pub fn new(
+        // Основные средства
+        fixed_assets: Money,
+        // Нематериальные активы
+        non_material_assets: Money,
+        // Финансовые вложения
+        financial_assets: Money,
+        // Прочее
+        other: Money,
+    ) -> Self {
+        Self {
+            fixed_assets,
+            non_material_assets,
+            financial_assets,
+            other,
+        }
+    }
+
+    pub fn total(&self) -> Money {
+        self.fixed_assets + self.non_material_assets + self.financial_assets + self.other
+    }
+}
+
+impl CurrentAssets {
+    pub fn new(
+        physical_inventory: Money,
+        accounts_receivable: Money,
+        cash: Money,
+        financial_assets: Money,
+        other: Money,
+    ) -> Self {
+        Self {
+            physical_inventory,
+            accounts_receivable,
+            cash,
+            financial_assets,
+            other,
+        }
+    }
+
+    pub fn total(&self) -> Money {
+        self.physical_inventory
+            + self.accounts_receivable
+            + self.cash
+            + self.financial_assets
+            + self.other
+    }
+}
+
+impl Assets {
+    pub fn new(current: CurrentAssets, non_current: NonCurrentAssets) -> Self {
+        Self {
+            current,
+            non_current,
+        }
+    }
+
+    pub fn total(&self) -> Money {
+        self.current.total() + self.non_current.total()
+    }
+}
+
+impl Equity {
+    pub fn new(
+        authorised_capital: Money,
+        capital_surplus: Money,
+        retained_earnings: Money,
+    ) -> Self {
+        Self {
+            authorised_capital,
+            capital_surplus,
+            retained_earnings,
+        }
+    }
+
+    pub fn total(&self) -> Money {
+        self.authorised_capital + self.capital_surplus + self.retained_earnings
+    }
+}
+
+impl LongTermLiabilities {
+    pub fn new(loans: Money, accounts_receivable: Money, other: Money) -> Self {
+        Self {
+            loans,
+            accounts_receivable,
+            other,
+        }
+    }
+    pub fn total(&self) -> Money {
+        self.loans + self.accounts_receivable + self.other
+    }
+}
+
+impl CurrentLiabilities {
+    pub fn new(loans: Money, accounts_receivable: Money, other: Money) -> Self {
+        Self {
+            loans,
+            accounts_receivable,
+            other,
+        }
+    }
+    pub fn total(&self) -> Money {
+        self.loans + self.accounts_receivable + self.other
+    }
+}
+
+impl Liabilities {
+    pub fn new(long_term: LongTermLiabilities, current: CurrentLiabilities) -> Liabilities {
+        Self { long_term, current }
+    }
+    pub fn total(&self) -> Money {
+        self.current.total() + self.long_term.total()
+    }
+}
+
+impl BalanceReport {
+    fn new(assets: Assets, equity: Equity, liabilities: Liabilities) -> Result<Self> {
+        if assets.total() != equity.total() + liabilities.total() {
+            return Err(eyre!(
+                "Balance mismatch. Assets {} Equity {} Liabilities {}",
+                assets.total(),
+                equity.total(),
+                liabilities.total()
+            ));
+        }
+        Ok(Self {
+            assets,
+            equity,
+            liabilities,
+        })
+    }
 }
--- src/model/money.rs
+++ src/model/money.rs
@@ -1,3 +1,22 @@
+use std::fmt;
+use std::ops;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Money {
     roubles: i64,
+}
+
+impl fmt::Display for Money {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ₽", self.roubles)
+    }
+}
+
+impl ops::Add for Money {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Self {
+            roubles: self.roubles + rhs.roubles,
+        }
+    }
 }
