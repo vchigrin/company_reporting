e276b167667701613c856862bb05d34714f29b2c
Vyacheslav Chigrin <vyacheslav.chigrin@izba.dev>
2026-01-31 20:07:12 +0300

    Add command-line args parsing.

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use clap::{Args, Parser, Subcommand, ValueEnum};
+use color_print::cprintln;
+use eyre::{Result, eyre};
+use std::path;
+
+mod period;
+
+#[derive(Debug, Clone, ValueEnum)]
+enum ReportType {
+    Balance,
+    Income,
+}
+
+#[derive(Debug, Args)]
+struct UpdateDbArgs {
+    #[arg(long)]
+    company_name: String,
+    #[arg(long, value_parser=period::Period::from_short_string)]
+    period: period::Period,
+    #[arg(long)]
+    report_type: ReportType,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    UpdateDb(UpdateDbArgs),
+}
+
+#[derive(Debug, Parser)]
+#[command(
+    version,
+    about = "Tool for parsing and evaluation company financial reports"
+)]
+struct CliParams {
+    #[command(subcommand)]
+    command: Command,
+    #[arg(long("db"))]
+    db_path: path::PathBuf,
+}
+
+fn handle_command(db_path: &path::Path, command: &Command) -> Result<()> {
+    Ok(())
+}
+
+fn do_main() -> Result<()> {
+    let params = CliParams::parse();
+    println!("Running with params {:?}", params);
+    handle_command(&params.db_path, &params.command)?;
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = do_main() {
+        println!("Error {}", e);
+        std::process::exit(1);
+    }
+}
---
+++ src/period.rs
@@ -0,0 +1,74 @@
+use eyre::{Result, eyre};
+use std::str::FromStr;
+
+#[derive(PartialOrd, PartialEq, Eq, Ord, Copy, Clone, Debug)]
+pub enum PeriodType {
+    FirstHalf,
+    // Non-standard, constructed from Full and FirstHalf periods.
+    SecondHalf,
+    Full,
+}
+
+#[derive(PartialOrd, PartialEq, Eq, Ord, Copy, Clone, Debug)]
+pub struct Period {
+    year: i32,
+    period_type: PeriodType,
+}
+
+impl Period {
+    pub fn first_half(year: i32) -> Self {
+        Self {
+            year,
+            period_type: PeriodType::FirstHalf,
+        }
+    }
+
+    pub fn second_half(year: i32) -> Self {
+        Self {
+            year,
+            period_type: PeriodType::SecondHalf,
+        }
+    }
+
+    pub fn full(year: i32) -> Self {
+        Self {
+            year,
+            period_type: PeriodType::Full,
+        }
+    }
+
+    pub fn period_type(&self) -> PeriodType {
+        self.period_type
+    }
+
+    pub fn short_string(&self) -> String {
+        let mut result = String::new();
+        result.push_str(&self.year.to_string());
+        match self.period_type {
+            PeriodType::FirstHalf => {
+                result += "H1";
+            }
+            PeriodType::SecondHalf => {
+                result += "H2";
+            }
+            PeriodType::Full => {
+                result += "FULL";
+            }
+        }
+        result
+    }
+
+    pub fn from_short_string(s: &str) -> Result<Self> {
+        if let Some(non_digit) = s.find(|c: char| !c.is_ascii_digit()) {
+            let year_str = &s[..non_digit];
+            let suffix = &s[non_digit..];
+            let year = i32::from_str(year_str)?;
+            if suffix == "H1" {
+                return Ok(Self::first_half(year));
+            } else if suffix == "FULL" {
+                return Ok(Self::full(year));
+            }
+        }
+        Err(eyre!("Not parsable period string {}", s))
+    }
+}
